480112584db02159bb8b846c3964f16ae1cae5ee
Kristy VanDerMolen <113852242+Kristyvdm2020@users.noreply.github.com>
2023-09-27 16:16:48 -0400

    Time: 221 ms (5.04%), Space: 52.7 MB (6.40%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let total = 0;
+    let nums = {
+        'I': 1,
+        'V': 5, 
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    for(let i = 0; i < s.length; i++) {
+        const current = s[i];
+        const next = s[i+1];
+        if(nums[next] > nums[current]) {
+            //console.log(nums[next] - nums[current]);
+            total += nums[next] - nums[current];
+            i++;
+        } else {
+            total += nums[current];
+        }
+        console.log(total);
+    }
+    return total;
+};
